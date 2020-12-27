f6eb18555e3e9a79ba07e274dc877dd192dc3631
Elena Kichinskaia <nitress@gmail.com>
2020-12-27 18:07:31 +0200

    task-5 completed

--- js/task-4.js
+++ js/task-4.js
@@ -7,6 +7,28 @@
 // Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
 // Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
 // Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+class StringBuilder {
+    constructor(value) {
+        this._value = value;
+    }
+    
+    get value() { 
+        return this._value
+    }
+
+    append(str) { 
+        this._value = this._value + str;
+        return this._value;
+    }
+    prepend(str) { 
+        this._value = str + this._value;
+        return this._value;
+    }
+    pad(str) {
+        this._value = str + this._value + str;
+        return this._value;
+    }
+}
 
 const builder = new StringBuilder('.');
 
